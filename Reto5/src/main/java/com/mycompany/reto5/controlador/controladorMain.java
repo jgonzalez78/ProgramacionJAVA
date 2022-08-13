c15cd3016e2fee574e8d4dba29832126c25b2134
jgonzalez78 <106284343+jgonzalez78@users.noreply.github.com>
2022-08-13 13:02:31 -0500

    Add files via upload

---
+++ Reto5/src/main/java/com/mycompany/reto5/controlador/controladorMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto5.controlador;
+
+import com.mycompany.reto5.vista.frmPrincipal;
+
+/**
+ *
+ * @author PC
+ */
+public class controladorMain {
+    public static void main(String[] args) {
+           
+        new frmPrincipal().setVisible(true);
+    }
+}
